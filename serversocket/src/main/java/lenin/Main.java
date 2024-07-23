009e6329df0a61465324a64b88eefc18f4336adc
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-07-23 09:27:51 -0500

    sync

--- serversocket/src/main/java/lenin/Main.java
+++ serversocket/src/main/java/lenin/Main.java
@@ -1,11 +1,26 @@
 package lenin;
 
+import java.net.ServerSocket;
+
+import lenin.java_server_socket.JavaServerSocket;
+
 /**
  * Hello world!
  *
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Java Server Socket");
+        
+        JavaServerSocket javaServerSocket = new JavaServerSocket(1802, 100);
+        ServerSocket serverSocket = javaServerSocket.get();
+        if (serverSocket == null) {
+            System.out.println("ServerSocket is null");
+            return;
+        }
+
+
+
+
     }
 }
---
+++ serversocket/src/main/java/lenin/server/Server.java
@@ -0,0 +1,31 @@
+package lenin.server;
+
+import java.util.List;
+
+public class Server implements SocketProcess{
+
+  @Override
+  public boolean bind() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'bind'");
+  }
+
+  @Override
+  public List<Object> listen() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'listen'");
+  }
+
+  @Override
+  public boolean response(List<Object> data) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'response'");
+  }
+
+  @Override
+  public boolean close() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'close'");
+  }
+  
+}
