210c8055d705ba55587869f8cd1ba4c04d827567
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-07-25 10:51:44 -0500

    sync

--- serversocket/src/main/java/lenin/server/Server.java
+++ serversocket/src/main/java/lenin/server/Server.java
@@ -30,6 +30,30 @@ public class Server implements SocketProcess{
 
   @Override
   public List<Object> listen() {
+    ArrayList<Object> dataList = new ArrayList<>();
+		boolean next = true;
+		Object data = null;
+		int flag = 1;
+		while (next) {
+			data = this.session.read();
+			if (data != null) {
+        try {
+          flag = (int) data;
+        } catch (Exception e) {
+          flag = 1;
+        }
+				try {					
+					next = flag != 0;
+					if (next) {
+						dataList.add(data);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+    }
+
+		return dataList;
   }
 
   @Override
