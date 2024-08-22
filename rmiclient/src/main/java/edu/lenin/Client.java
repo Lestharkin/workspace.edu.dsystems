50b57722e5a9112c024338031cf32c4398b5171c
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-08-22 08:37:44 -0500

    sync

--- rmiclient/src/main/java/edu/lenin/Client.java
+++ rmiclient/src/main/java/edu/lenin/Client.java
@@ -1,16 +1,18 @@
 package edu.lenin;
 
+import java.rmi.Naming;
+
 public class Client {
 
   private String url;
 
   public Client(String ip, String port, String serviceName) {
-    this.uri = "rmi://" + ip + ":" + port + "/" + serviceName;
+    this.url = "rmi://" + ip + ":" + port + "/" + serviceName;
   }
 
   public String sayHello(String msg) {
     try {
-      RMIMSG service = (RMIMSG) Naming.lookup(url);
+      RMIMSG service = (RMIMSG) Naming.lookup(this.url);
       return service.sayHello(msg);
     } catch (Exception e) {
       e.printStackTrace();
